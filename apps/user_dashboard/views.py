082101f04fee1493ab9f327b85f8e63eb5b98161
Oladimeji Oladipo <jedi132000@gmail.com>
2019-04-24 15:58:04 -0400

    added templates

--- apps/user_dashboard/views.py
+++ apps/user_dashboard/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import User, Post
+from django.contrib import messages
 # Create your views here.
+
+def index(request):
+    return render(request, 'user_dashboard/index.html')
+
+
+def register(request):
+    return render(request, 'user_dashboard/register.html')
+
+def create(request):
+    return redirect('/signin')
+
+def signin(request):
+    return render(request, 'user_dashboard/signin.html')
+
+def login(request):
+    return redirect('/dashboard')
+
+
+def dashboard(request):
+    return render(request, 'user_dashboard/dashboard.html')
+
+def admin_dashboard(request):
+    return render(request, 'user_dashboard/admin-dashboard.html')
+
+def new_user(request):
+    return render(request, 'user_dashboard/new.html')
+

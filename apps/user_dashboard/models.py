b60ee5cbd1a20f8258b0aab49d4b7e15dd48a842
Oladimeji Oladipo <jedi132000@gmail.com>
2019-04-24 17:02:29 -0400

    fat models

--- apps/user_dashboard/models.py
+++ apps/user_dashboard/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
+    email = models.CharField(max_length=255)
+    user_level = models.CharField(max_length=45, default='normal')
+    pw_hash = models.CharField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Post(models.Model):
+    post_sender = models.ForeignKey(User, related_name='poster')
+    post_reciever = models.ForeignKey(User, related_name='postee')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
--- apps/user_dashboard/views.py
+++ apps/user_dashboard/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-# from .models import User, Post
+from .models import User, Post
 from django.contrib import messages
 # Create your views here.
 
